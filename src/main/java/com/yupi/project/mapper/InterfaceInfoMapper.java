aaf45b53549fab51f3326096b546b68ed44dfc38
advent.ni <nly2455158786@163.com>
2025-09-02 17:19:41 +0800

    first commit

---
+++ src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.InterfaceInfo;
+
+/**
+* @author Netis
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2025-09-02 16:32:18
+* @Entity com.yupi.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/project/service/InterfaceInfoService.java
@@ -0,0 +1,15 @@
+package com.yupi.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.yupi.project.model.entity.Post;
+
+/**
+* @author Netis
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2025-09-02 16:32:18
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+    void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add);
+}
